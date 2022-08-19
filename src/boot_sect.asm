f88753e408761fc219eaa7c734acc1ac52fbb65c
Di0go <diogopinto301@gmail.com>
2022-08-19 04:19:56 +0100

    Pointers, Offsets and Memory Structure

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -1,20 +1,36 @@
+[org 0x7c00]
+
 ; tty mode
 mov ah, 0x0E
 
-; print oops
-mov al, "o"
+mov al, "1"
 int 0x10
-; repeat because "o" is still in al
-int 0x10
-
-mov al, "p"
+mov al, the_secret
 int 0x10
 
-mov al, "s"
+mov al, "2"
+int 0x10
+mov al, [the_secret]
+int 0x10
+
+mov al, "3"
+int 0x10
+mov bx, the_secret
+add bx, 0x7C00
+mov al, [bx]
+int 0x10
+
+mov al, "4"
+int 0x10
+mov al, [0x7c2d]
 int 0x10
 
 ; jmp to current address (hang in infinite loop)
 jmp $
+
+; label
+the_secret:
+    db "X"
 
 ; fill all unused bytes with 0
 times 510-($-$$) db 0
